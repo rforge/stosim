2fa3a3e111cc5a448decfa1edfc5a4e3ce9c5259
jtormerod <jtormerod@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-03-21 15:07:57 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/stosim@3 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/stosim/src/stosim.h
@@ -0,0 +1,34 @@
+// stosim.h file
+ /*
+ * Author: Jacob T. Ormerod
+ *         (c) 2014 OpenReliability.org
+ */
+
+#ifndef _stosim_H
+#define _stosim_H
+
+#include <Rcpp.h>
+
+RcppExport SEXP withStorage(SEXP a, SEXP b, SEXP c);
+
+RcppExport  SEXP SimulationHistory(SEXP arg1, SEXP arg2, SEXP arg3,
+      SEXP arg4,  SEXP arg5, SEXP arg6,
+        SEXP arg7,  SEXP arg8, SEXP arg9,
+          SEXP arg10,  SEXP arg11, SEXP arg12);
+
+ RcppExport  SEXP addWpush(SEXP arg1, SEXP arg2, SEXP arg3,
+      SEXP arg4,  SEXP arg5, SEXP arg6,
+        SEXP arg7,  SEXP arg8);
+
+  RcppExport  SEXP addOverlay(SEXP arg1, SEXP arg2, SEXP arg3,
+      SEXP arg4,  SEXP arg5, SEXP arg6,
+        SEXP arg7,  SEXP arg8);
+        
+   RcppExport  SEXP DetailOpLines(SEXP arg1, SEXP arg2, SEXP arg3,
+      SEXP arg4); 
+      
+   RcppExport  SEXP MultiTrainSingleBU(SEXP arg1, SEXP arg2, SEXP arg3,
+      SEXP arg4,  SEXP arg5, SEXP arg6,
+        SEXP arg7,  SEXP arg8);          
+
+#endif
