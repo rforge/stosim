3165eb841b6d89a9910c46970e4ad6919510853d
jtormerod <jtormerod@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-04-10 02:51:16 +0000

    Added MultiTrainSingleBU source only, conditioned Rcpp to > version 0.11
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/stosim@9 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/stosim/src/stosim.h
+++ pkg/stosim/src/stosim.h
@@ -27,7 +27,7 @@ RcppExport  SEXP SimulationHistory(SEXP arg1, SEXP arg2, SEXP arg3,
    RcppExport  SEXP DetailOpLines(SEXP arg1, SEXP arg2, SEXP arg3,
       SEXP arg4); 
       
-   RcppExport  SEXP MultiTrainSingleBU(SEXP arg1, SEXP arg2, SEXP arg3,
+   RcppExport  SEXP MultiTrainSingleBU4a(SEXP arg1, SEXP arg2, SEXP arg3,
       SEXP arg4,  SEXP arg5, SEXP arg6,
         SEXP arg7,  SEXP arg8);          
 
